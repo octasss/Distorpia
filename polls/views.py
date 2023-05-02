4dc084e29ae5a6970efd67d2e8cf31cab9626a75
Octávio Casarini <o.casarini@aluno.ifsp.edu.br>
2023-05-02 10:22:04 -0300

    delete

--- polls/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def index(request):
-    return HttpResponse("Olá Mundo! Esta é a página inicial do aplicativo.")
